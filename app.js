80f9b5da823db781e616f0959d7327d3f82df838
prtvi <prithvippv25@gmail.com>
2022-03-15 01:26:09 +0530

    add crop-sug, fertilizer-sug & precision-irr routes
    add js/scripts to get click events on home route, add 'routeName' data attribute to get route-name

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(express.static("public"));
 
 const { products, featureIcons, homeProducts } = require("./data.js");
 
+// 
+//
+
 // HOME ROUTE
 app.get("/", (req, res) => {
   res.render("index", {
@@ -18,6 +21,9 @@ app.get("/", (req, res) => {
     featureIcons,
   });
 });
+
+//
+//
 
 // LOGIN ROUTE
 app.get("/login", (req, res) => {
@@ -32,6 +38,9 @@ app.post("/login", (req, res) => {
   res.redirect("/home");
 });
 
+//
+//
+
 // REGISTER ROUTE
 app.get("/register", (req, res) => {
   res.render("register");
@@ -42,13 +51,45 @@ app.post("/register", (req, res) => {
 
   console.log(email, password);
 
-  res.redirect("/");
+  res.redirect("/home");
 });
 
+//
+//
+
+// HOME ROUTE (AFTER AUTH)
 app.get("/home", (req, res) => {
   res.render("home", { homeProducts });
 });
 
+//
+//
+
+// PRECISION IRRIGATION (AFTER AUTH)
+app.get("/precisionIrrigation", (req, res) => {
+  res.render("precisionIrrigation");
+});
+
+//
+//
+
+// CROP SUGGESTION (AFTER AUTH)
+app.get("/cropSuggestion", (req, res) => {
+  res.render("cropSuggestion");
+});
+
+//
+//
+
+// FERTILIZER SUGGESTION (AFTER AUTH)
+app.get("/fertilizerSuggestion", (req, res) => {
+  res.render("fertilizerSuggestion");
+});
+
+//
+//
+
+// SERVER RUNNING
 app.listen(PORT, () => {
   console.log(`Server is runnning on port ${PORT}!`);
 });
