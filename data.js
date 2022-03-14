d6a472f823bde3c3a009da88d7e454276c0a6c4e
prtvi <prithvippv25@gmail.com>
2022-03-15 01:26:09 +0530

    add crop-sug, fertilizer-sug & precision-irr routes, #9
    add js/scripts to get click events on home route, add 'routeName' data attribute to get route-name

--- data.js
+++ data.js
@@ -51,23 +51,27 @@ exports.featureIcons = [
 ];
 
 class HomeProduct {
-  constructor(title, description) {
+  constructor(title, description, routeName) {
     this.title = title;
     this.description = description;
+    this.routeName = routeName;
   }
 }
 
 exports.homeProducts = [
   new HomeProduct(
     "Precision Irrigation",
-    "+ click here to explore the automated irrigation system."
+    "+ click here to explore the automated irrigation system.",
+    "precisionIrrigation"
   ),
   new HomeProduct(
     "Crop Suggestion",
-    "+ click here to find out the best crop to grow on your land."
+    "+ click here to find out the best crop to grow on your land.",
+    "cropSuggestion"
   ),
   new HomeProduct(
     "Fertilizer Suggestion",
-    "+ click here to find out the fertilizer / manure required for your field."
+    "+ click here to find out the fertilizer / manure required for your field.",
+    "fertilizerSuggestion"
   ),
 ];
