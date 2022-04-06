c2a31da734a059c621121f70308f7ec091958ed3
gokulbalaji <gokulbalaji.sjce@gmail.com>
2022-04-06 23:41:21 +0530

    more sensors and date&time update

--- harwareUpdates/mainCode.py
+++ harwareUpdates/mainCode.py
@@ -1,40 +1,76 @@
 from pymongo import MongoClient
+from datetime import datetime
 import pyfirmata 
 import time 
 
+
 board = pyfirmata.Arduino('COM4')
-client = MongoClient("mongodb+srv://XXXX:XXXX@sensordata.4x7l6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://test_user:test_user@sensordata.4x7l6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 it = pyfirmata.util.Iterator(board)
 it.start()
 
 i = 0
-gas_avg = 0
-rain_avg = 0
+
+soil_hum_avg = 0
+soil_ph_avg = 0
+air_temp_avg = 0
+air_hum_avg = 0
+rain_val_avg = 0
 
 while True:
     board.analog[0].enable_reporting()
     board.analog[1].enable_reporting()
-    rain_val = board.analog[0].read()
-    gas_val = board.analog[1].read()
-    if gas_val == None:
-        gas_val = 0
-    if rain_val == None:
-        rain_val = 0
-    gas_avg = gas_avg + gas_val
-    rain_avg = rain_avg + rain_val
+    board.analog[2].enable_reporting()
+    board.analog[3].enable_reporting()
+    board.analog[4].enable_reporting()
+    
+    soil_hum = board.analog[0].read()
+    soil_ph = board.analog[1].read()
+    air_temp = board.analog[2].read()
+    air_hum = board.analog[3].read()
+    rain_val = board.analog[4].read()
+
+    params = [soil_hum, soil_ph, air_temp, air_temp, air_hum, rain_val]
+    
+    for x in params:
+        if (x == None):
+            x = 0
+            
+    soil_hum_avg = soil_hum_avg + soil_hum
+    soil_ph_avg = soil_ph_avg + soil_ph
+    air_temp_avg = air_temp_avg + air_temp
+    air_hum_avg = air_hum_avg + air_hum
+    rain_val_avg = rain_val_avg + rain_val
+
     #print(gas_val)
+
+
     i = i + 1
     if(i==10):
-        gas_avg = '%.3f'%(gas_avg/10)
-        rain_avg = '%.3f'%(rain_avg/10)
+        soil_hum_avg= '%.3f'%(soil_hum_avg/10)
+        soil_ph_avg= '%.3f'%(soil_ph_avg/10)
+        air_temp_avg= '%.3f'%(air_temp_avg/10)
+        air_hum_avg= '%.3f'%(air_hum_avg/10)
+        rain_val_avg= '%.3f'%(rain_val_avg/10)
 
-        print(f"Average gas value : {gas_avg}")
-        print(f"Average rain value : {rain_avg}")
+
+        print(f"Average soil humidity value : {soil_hum_avg}")
+        print(f"Average soil ph value : {soil_ph_avg}")
+        print(f"Average air temperature value : {air_temp_avg}")
+        print(f"Average air humidity value : {air_hum_avg}")
+        print(f"Average rain fall value : {rain_val_avg}")
+
         db = client.get_database('sensor_data') #db Name
         record = db.sensor_records  #document name
-        new_val = {'gas_val' : str(gas_avg),
-            'rain_val' : str(rain_avg)}
+        when = datetime.now().strftime("%d-%m-%y %H:%M")
+        new_val = {
+            'when' : when,
+            'soil_hum' : str(soil_hum_avg),
+            'soil_ph' : str(soil_ph_avg),
+            'air_temp' : str(air_temp_avg),
+            'air_hum' : str(air_hum_avg),
+            'rain_val' : str(rain_val_avg)}
         record.insert_one(new_val)
         exit()
     time.sleep(0.5)
